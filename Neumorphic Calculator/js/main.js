f5c7d771a6bee7f86eae6b4956f9d2b44357638e
crazyAT8 <80292649+crazyAT8@users.noreply.github.com>
2021-06-22 00:28:34 -0500

    Add files via upload

---
+++ Neumorphic Calculator/js/main.js
@@ -0,0 +1,90 @@
+//need to target our input
+const $input = document.querySelector("input");
+
+//now we are going to loop through all the number keys and put an event listener on them
+document.querySelectorAll(".num__key").forEach(    //arrow function
+    el => {
+//element on click--another arrow function
+        el.onclick = () => ($input.value = 
+                                            //this is another way to write an if statement
+        $input.value !== "0"                //if the current input value is not equal to zero
+        ? $input.value + el.innerText :     //we want the value to be the number or inner text of the element, to concatinate with another inner text
+        el.innerText);                       //if the element is zero then we want to replace it with the elements number or inner text
+                                            //take note that we are dealing with text not actual numbers
+    });
+
+//need to set up a buffer
+        //keep an empty array to keep track of the numbers that have been entered along with the operations performed
+const buffer = [];
+
+//this is the specific call back
+const opCallback = opName => () => {
+    let currentVal = parseFloat($input.value);      //this converts the text to a number
+
+    if (opName === "percent") {                     //this is distinguishing which operator
+        currentVal *= 0.01;                         //this is the conversion for geting a percentage
+        $input.value = currentVal;                  //this is taking what was entered and operated on as the new value being displayed
+    }
+    else {                                          //if its not a percentage then we are going to add it to the buffer
+        if (buffer && buffer.length) {              //if there is a value and operation in the buffer
+            buffer.push({ value: currentVal});      //enter another value to the buffer
+
+            const result = evaluate(buffer);         //performs the operation of any number of entries
+
+            buffer.push({ value: result });         //displays the result of the last entry
+            buffer.push({ value: opName });         //enter the next operation to be taken, if any
+
+            $input.value = "";                      //clears the input display
+        }
+        else {
+            buffer.push({ value: currentVal });     //puts whatever is in the buffer with the operation
+            buffer.push({ value: opName });         //takes the operation selected
+            $input.value = ""                       //clears the input display
+        }
+    }
+}
+                                                    //remember that "pop" pulls the last value out of an array
+const evaluate = buffer => {
+    const secondOperand = buffer.pop().value;
+    const operator = buffer.pop().value;
+    const firstOperand = buffer.pop().value;
+
+    switch (operator) {
+        case "add":
+            return firstOperand + secondOperand;
+            break;
+        case "subtract":
+            return firstOperand - secondOperand;
+            break;
+        case "multiply":
+            return firstOperand * secondOperand;
+            break;
+        case "divide":
+            return firstOperand / secondOperand;
+            break;
+        default:
+            return secondOperand;
+    }
+}
+
+for (const opName of ["add", "subract", "multiply", "divide", "percent"]) {     //we are looping through the array of operations
+    document.querySelector('.op__key[op=${opName}]')                            //selecting an operations key with the corrisponding name to be used
+    .onclick = opCallback(opName);                                              //onclick event listener, so every click is a call back, and whatever was clicked will be the specific name of the operation
+}
+
+//Equals button 
+document.querySelector(".eq__key").onclick = 
+    () => {
+        if (buffer && buffer.length) {
+            buffer.push({ value: parseFloat($input.value)
+            });
+            $input.value = evaluate(buffer);
+        }
+    };
+
+//clear button
+document.querySelector(".op__key[op=clear]")
+.onclick = () => {
+    $input.value = 0;               
+    buffer.length = 0;                  //when you set the length of an array to zero, you are basically erasing it
+    };
